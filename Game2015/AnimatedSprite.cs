937fc5547734e4fa637ce99afbe5eb7b514269c0
Championnick <nick_diktakis@live.ca>
2015-03-29 15:49:33 -0400

    Edited more stuff

--- Game2015/AnimatedSprite.cs
+++ Game2015/AnimatedSprite.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System.Diagnostics;
 
 namespace Game2015
 {
@@ -11,9 +12,12 @@ namespace Game2015
     abstract class AnimatedSprite 
     {
 
+        public enum myDirection { NONE, LEFT, RIGHT, UP, DOWN };
+
+        protected myDirection currentDirection = myDirection.NONE;
+
         protected Texture2D sTexture;
-        private Vector2 sPosition;
-        private Rectangle[] sRectangles;
+        protected Vector2 sPosition;
 
        // private int xFrameIndex;
         //private int yFrameIndex;
@@ -22,30 +26,42 @@ namespace Game2015
         private double timeElapsed;
         private double timeToUpdate;
 
+        protected string currentAnimation;
+
+        protected Vector2 sDirection = Vector2.Zero;
+
         public int FramesPerSecond
         {
             set { timeToUpdate = (1f / value); }
         }
+
+        private Dictionary<string, Rectangle[]> sAnimations = new Dictionary<string, Rectangle[]>();
+        private Dictionary<string, Vector2> sOffSets = new Dictionary<string, Vector2>();
+
 
         public AnimatedSprite(Vector2 position)
         {
             sPosition = position;
         }
 
-        public void AddAnimation(int frames)
+
+        public void AddAnimation(int frames, int yPosition, int xStartFrame, string name, int sWidth, int sHeight, Vector2 offSet)
         {
-            int width = sTexture.Width / frames;
 
-            sRectangles = new Rectangle[frames];
+            //Creates an array of rectangles which will be used when playing an animation
+            Rectangle[] Rectangles = new Rectangle[frames];
 
-            for (int i = 0; i < frames; ++i )
+            //Fills up the array of rectangles
+            for (int i = 0; i < frames; i++)
             {
-                sRectangles[i] = new Rectangle(i * width, 0, width, sTexture.Height);
-
+                Rectangles[i] = new Rectangle((i + xStartFrame) * sWidth, yPosition, sWidth, sHeight);
             }
 
+            sAnimations.Add(name, Rectangles);
+            sOffSets.Add(name, offSet);
+            Debug.WriteLine(sAnimations[name].Length);
         }
-        public void Update(GameTime gameTime)
+        public virtual void Update(GameTime gameTime)
         {
             timeElapsed += gameTime.ElapsedGameTime.TotalSeconds;
 
@@ -53,12 +69,13 @@ namespace Game2015
             {
                 timeElapsed -= timeToUpdate;
 
-                if(frameIndex < sRectangles.Length - 1)
+                if(frameIndex < sAnimations[currentAnimation].Length - 1)
                 {
                     frameIndex++;
                 }
                 else
                 {
+                    AnimationDone(currentAnimation);
                     frameIndex = 0;
                 }
             }
@@ -67,9 +84,21 @@ namespace Game2015
         public void Draw(SpriteBatch spriteBatch)
         {
 
-            spriteBatch.Draw(sTexture, sPosition, sRectangles[frameIndex], Color.White);
+            spriteBatch.Draw(sTexture, sPosition + sOffSets[currentAnimation], sAnimations[currentAnimation][frameIndex], Color.White);
 
         }
+
+        public void PlayAnimation(string name)
+        {
+            if(currentAnimation != name && currentDirection == myDirection.NONE)
+            {
+                currentAnimation = name;
+                frameIndex = 0;
+            }
+        }
+
+        public abstract void AnimationDone(string animation);
+       
 
 
     }
